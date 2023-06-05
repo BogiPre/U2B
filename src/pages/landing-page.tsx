4d367b1d837191dd6cb4b93eec1b269f150c4094
BogiPre <bogicevic.predrag1@gmail.com>
2023-06-05 14:36:55 +0200

    Erster Push

---
+++ src/pages/landing-page.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+    return (
+      <div class="flex items-center justify-center h-full w-full">
+        <h1 class="text-4xl">Welcome to this Youtube playlist & videos to mp3 converter!</h1>
+      </div>
+    );
+  }
+  
