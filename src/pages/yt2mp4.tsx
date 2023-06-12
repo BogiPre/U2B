d71457e7a1cecb4b1478620e2f0ed5d3c9383426
BogiPre <bogicevic.predrag1@gmail.com>
2023-06-12 14:08:57 +0200

    alert added

--- src/pages/yt2mp4.tsx
+++ src/pages/yt2mp4.tsx
@@ -6,90 +6,110 @@ import InputBox from "../components/InputBox";
 import DownloadList from "../components/DownloadList";
 import DownloadListEntry from "../components/DownloadListEntry";
 
-const [downloadList, setDownloadList] = createSignal(new Array<DownloadListEntry>());
+const [downloadList, setDownloadList] = createSignal(
+  new Array<DownloadListEntry>()
+);
 
 // YouTube API fetch functions
 const fetchYouTubeAPI: any = async (givenUrl: string) => {
-    const playlistId = givenUrl.slice(givenUrl.indexOf("playlist?list=") + "playlist?list=".length);
-    const videoId = givenUrl.slice(givenUrl.indexOf("watch?v=") + "watch?v=".length);
+  const youtubeUrlRegex =
+    /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+  if (!youtubeUrlRegex.test(givenUrl)) {
+    alert(
+      "Invalid YouTube URL. Please enter a valid YouTube video or playlist URL."
+    );
+    return;
+  }
 
-    if (givenUrl.includes("playlist?list=")) {
-        const options = {
-            method: 'GET',
-            url: 'https://yt-api.p.rapidapi.com/playlist',
-            params: {
-                id: playlistId
-            },
-            headers: {
-                'X-RapidAPI-Key': 'c7ab3deda8msh2451a1693896b23p1a6f98jsnab2367e95718',
-                'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
-            }
-        };
+  const playlistId = givenUrl.slice(
+    givenUrl.indexOf("playlist?list=") + "playlist?list=".length
+  );
+  const videoId = givenUrl.slice(
+    givenUrl.indexOf("watch?v=") + "watch?v=".length
+  );
 
-        try {
-            const response = await axios.request(options);
-            response.data.data.forEach((element: any) => {
-                setTimeout(() => {
-                    fetchYouTubeAPI("https://www.youtube.com/watch?v=" + element.videoId);
-                }, 5000);
-            });
-        } catch (error) {
-            console.error(error);
-        }
-        return {};
-    }
-
+  if (givenUrl.includes("playlist?list=")) {
     const options = {
-        method: 'GET',
-        url: 'https://yt-api.p.rapidapi.com/dl',
-        params: { id: videoId },
-        headers: {
-            'X-RapidAPI-Key': 'c7ab3deda8msh2451a1693896b23p1a6f98jsnab2367e95718',
-            'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
-        }
+      method: "GET",
+      url: "https://yt-api.p.rapidapi.com/playlist",
+      params: {
+        id: playlistId,
+      },
+      headers: {
+        "X-RapidAPI-Key": "c7ab3deda8msh2451a1693896b23p1a6f98jsnab2367e95718",
+        "X-RapidAPI-Host": "yt-api.p.rapidapi.com",
+      },
     };
 
     try {
-        const response = await axios.request(options);
-        const url = response.data.formats[selectedOption().index].url;
-
-        console.log(response.data.formats);
-
-        setDownloadList([...downloadList(), new DownloadListEntry(response.data.title, url)]);
-
-        return url;
+      const response = await axios.request(options);
+      response.data.data.forEach((element: any) => {
+        setTimeout(() => {
+          fetchYouTubeAPI("https://www.youtube.com/watch?v=" + element.videoId);
+        }, 5000);
+      });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
+    return {};
+  }
 
-}
+  const options = {
+    method: "GET",
+    url: "https://yt-api.p.rapidapi.com/dl",
+    params: { id: videoId },
+    headers: {
+      "X-RapidAPI-Key": "c7ab3deda8msh2451a1693896b23p1a6f98jsnab2367e95718",
+      "X-RapidAPI-Host": "yt-api.p.rapidapi.com",
+    },
+  };
 
+  try {
+    const response = await axios.request(options);
+    const url = response.data.formats[selectedOption().index].url;
 
+    console.log(response.data.formats);
+
+    setDownloadList([
+      ...downloadList(),
+      new DownloadListEntry(response.data.title, url),
+    ]);
+
+    return url;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // Component
 export default function InputBoxPage() {
-    const [inputValue, setInputValue] = createSignal("");
+  const [inputValue, setInputValue] = createSignal("");
 
-    const handleClick = async () => {
-        await fetchYouTubeAPI(inputValue());
-    };
+  const handleClick = async () => {
+    await fetchYouTubeAPI(inputValue());
+  };
 
-    const addToFavList = (event: MouseEvent) => {
-        const svg = event.currentTarget as HTMLElement;
-        const anchor = svg.parentElement?.querySelector("a") as HTMLAnchorElement;
-        const urlToSave = anchor?.href;
-        setFavList([...favList(), new FavListEntry(anchor.innerText, urlToSave)]);
-        localStorage.setItem("savedLinks", JSON.stringify(favList()));
-    }
+  const addToFavList = (event: MouseEvent) => {
+    const svg = event.currentTarget as HTMLElement;
+    const anchor = svg.parentElement?.querySelector("a") as HTMLAnchorElement;
+    const urlToSave = anchor?.href;
+    setFavList([...favList(), new FavListEntry(anchor.innerText, urlToSave)]);
+    localStorage.setItem("savedLinks", JSON.stringify(favList()));
+  };
 
-    return (
-        <main class="flex items-center justify-center min-h-screen">
-            <div class="flex flex-col items-center justify-center w-max mx-auto gap-8 p-8 bg-gray-50 h-min shadow-md">
-                <h1 class="text-4xl mb-4 text-center text-gray-700">Video or Playlist Url</h1>
-                <InputBox inputValue={inputValue} setInputValue={setInputValue} handleClick={handleClick} />
-                <DownloadList downloadList={downloadList} addToFavList={addToFavList} />
-            </div>
-        </main>
-
-    );
+  return (
+    <main class="flex items-center justify-center min-h-screen">
+      <div class="flex flex-col items-center justify-center w-max mx-auto gap-8 p-8 bg-gray-50 h-min shadow-md">
+        <h1 class="text-4xl mb-4 text-center text-gray-700">
+          Video or Playlist Url
+        </h1>
+        <InputBox
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          handleClick={handleClick}
+        />
+        <DownloadList downloadList={downloadList} addToFavList={addToFavList} />
+      </div>
+    </main>
+  );
 }
